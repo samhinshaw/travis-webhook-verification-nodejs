0180e949d0ddd4f8d2e46dee4f861daca067f455
Sam Hinshaw <samuel.hinshaw@gmail.com>
2019-01-24 19:32:46 -0800

    Update webhook fire to pull compose file

--- travis/fire-webhook.js
+++ travis/fire-webhook.js
@@ -1,18 +1,32 @@
 const { exec } = require('child_process');
 
+const { REPO_OWNER, REPO_NAME, REPO_COMPOSE_PATH, SERVER_DEPLOY_PATH } = require('./constants');
+
 function fireWebhook(script, options) {
+  // Check that this is a tagged release
   if (!options.tag) {
     console.error('Not a tagged release, not deploying.');
     return;
   }
-  exec(`bash ${script} ${options.tag}`, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec error: ${error}`);
-      return;
+
+  const composeFileURL = `https://raw.githubusercontent.com/${REPO_OWNER}/${REPO_NAME}/${
+    options.tag
+  }/${REPO_COMPOSE_PATH}`;
+
+  // Be very careful! The order of these arguments is vital
+  exec(
+    `bash ${script} ${
+      options.tag
+    } ${composeFileURL} ${SERVER_DEPLOY_PATH} ${REPO_OWNER} ${REPO_NAME}`,
+    (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+      console.error(`stderr: ${stderr}`);
     }
-    console.log(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
-  });
+  );
 }
 
 module.exports = fireWebhook;
