f23544ed7465d135c5f60f694494ec40e9e332c5
Sam Hinshaw <samuel.hinshaw@gmail.com>
2019-01-24 22:47:01 -0800

    Update dir to reflect mounted folder

--- travis/constants.js
+++ travis/constants.js
@@ -2,6 +2,6 @@ const SCRIPT_NAME = 'scripts/deploy-getfit.sh';
 const REPO_NAME = 'get_fit';
 const REPO_OWNER = 'samhinshaw';
 const REPO_COMPOSE_PATH = 'docker-compose.prod.yml';
-const SERVER_DEPLOY_PATH = '/home/sam/deploy/get_fit';
+const SERVER_DEPLOY_PATH = '/deploy/get_fit';
 
 module.exports = { SCRIPT_NAME, REPO_NAME, REPO_OWNER, REPO_COMPOSE_PATH, SERVER_DEPLOY_PATH };
