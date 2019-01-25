8680a3496ecca9643efce7f92d83010ee95d595c
Sam Hinshaw <samuel.hinshaw@gmail.com>
2019-01-24 19:31:29 -0800

    Update deploy script to pull compose file

--- scripts/deploy-getfit.sh
+++ scripts/deploy-getfit.sh
@@ -1,11 +1,23 @@
 #! /bin/bash
 
-docker pull samhinshaw/get_fit:$1
+# Be very careful! The order of these arguments is vital
+TAG=$1
+COMPUSE_FILE_URL=$2
+SERVER_DEPLOY_PATH=$3
+REPO_OWNER=$4
+REPO_NAME=$5
 
-docker run                 \
-  -d                       \
-  -p 8005:8005             \
-  -e "NODE_ENV=production" \
-  --env-file ./env.list    \
-  --restart=always         \
-  samhinshaw/get_fit:$1
+# exit if any command fails
+set -e
+
+# Make deploy dir if not already existing
+mkdir -p $DIR 
+
+# Get the correct docker compose file
+wget -q $URL -O $DIR/docker-compose.yml
+
+# Pull down the tagged image
+docker pull $REPO_OWNER/$REPO_NAME:$TAG
+
+# boot it up!
+docker-compose -f $DIR/docker-compose.yml up -d
