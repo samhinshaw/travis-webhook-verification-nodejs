a1dbcb144dfdce0a3cf11b49da792bd667616a10
Sam Hinshaw <samuel.hinshaw@gmail.com>
2019-01-25 16:02:09 -0800

    Deploy anything on release/ or tag on any branch

--- travis/check-should-fire-webhook.js
+++ travis/check-should-fire-webhook.js
@@ -2,21 +2,21 @@ const { REPO_NAME, REPO_OWNER } = require('./constants');
 
 function checkShouldFireWebHook(payload) {
   //   All of the following must be true:
-  //   - master or develop branches
+  //   - release/* branch (or tag on any branch)
   //   - a tag must be present
   //   - status & result must be 0 (meaning successful builds)
   //   - repository owner is "samhinshaw"
   //   - repository is "get_fit"
-  const isBranchDeployable = payload.branch === 'master' || payload.branch === 'develop';
+  const isBranchDeployable = payload.branch.startsWith('release/');
   const isTagPresent = !!payload.tag;
+  const isCommitDeployable = isBranchDeployable || isTagPresent;
+
   const isStatusSuccess = payload.status === 0 && payload.result === 0;
   const isWebhookForGetFit = payload.repository.name === REPO_NAME;
   const isWebhookMine = payload.repository.owner_name === REPO_OWNER;
 
   // Make sure ALL conditions are met
-  return (
-    isBranchDeployable && isTagPresent && isStatusSuccess && isWebhookMine && isWebhookForGetFit
-  );
+  return isCommitDeployable && isStatusSuccess && isWebhookMine && isWebhookForGetFit;
 }
 
 module.exports = checkShouldFireWebHook;
