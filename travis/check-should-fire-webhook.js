cca41a8c53cd879271bd2c911ab9396f6108a521
Sam Hinshaw <samuel.hinshaw@gmail.com>
2019-01-26 08:58:38 -0800

    Remove debug statements

--- travis/check-should-fire-webhook.js
+++ travis/check-should-fire-webhook.js
@@ -8,7 +8,6 @@ function checkShouldFireWebHook(payload) {
   //   - repository owner is "samhinshaw"
   //   - repository is "get_fit"
   try {
-    console.log(typeof payload.branch);
     const isBranchDeployable = payload.branch.startsWith('release/');
     const isTagPresent = !!payload.tag;
 
--- travis/handle-request.js
+++ travis/handle-request.js
@@ -6,14 +6,8 @@ const fireWebhook = require('./fire-webhook');
 const { SCRIPT_NAME } = require('./constants');
 
 function handleRequest(req, res, next) {
-  if (req.body) {
-    console.log(req.body);
-  } else {
-    console.log(req);
-  }
-
   if (!req.headers.signature || !req.body.payload) {
-    console.log('Invalid request sent.');
+    console.log('Request missing signature or payload.');
     res.sendStatus(400);
     next();
     return;
