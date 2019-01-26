8abe57b73fda5f21dee92a8f1210102b93e0f1f2
Sam Hinshaw <samuel.hinshaw@gmail.com>
2019-01-26 07:50:56 -0800

    Down preexisting containers before booting up new

--- scripts/deploy-getfit.sh
+++ scripts/deploy-getfit.sh
@@ -9,15 +9,28 @@ COMPOSE_FILE_URL=$2
 SERVER_DEPLOY_PATH=$3
 REPO_OWNER=$4
 REPO_NAME=$5
+CONTAINERS_DOWNED=false
 
 # Make deploy dir if not already existing
 mkdir -p $SERVER_DEPLOY_PATH
+
+# If a docker-compose file is already in our deploy path, down the containers with it
+# Note: DOCKER_TAG does not need to be specified for the 'down' command
+if [ -f $SERVER_DEPLOY_PATH/docker-compose.yml ]; then
+  CONTAINERS_DOWNED=true
+  docker-compose -f $SERVER_DEPLOY_PATH/docker-compose.yml down -v
+fi
 
 # Get the correct docker compose file
 wget -q $COMPOSE_FILE_URL -O $SERVER_DEPLOY_PATH/docker-compose.yml
 
 # Pull down the tagged image
 docker pull $REPO_OWNER/$REPO_NAME:$TAG
+
+# If we haven't already downed the currently-running containers, do so now
+if [ "$CONTAINERS_DOWNED" = false ]; then
+  docker-compose -f $SERVER_DEPLOY_PATH/docker-compose.yml down -v
+fi
 
 # boot it up!
 DOCKER_TAG=$TAG docker-compose -f $SERVER_DEPLOY_PATH/docker-compose.yml up -d
