0a0f977c46f48f2c415a4590e5bd2e92532de897
Sam Hinshaw <samuel.hinshaw@gmail.com>
2019-01-25 17:40:21 -0800

    Promisify webhook firing

--- travis/fire-webhook.js
+++ travis/fire-webhook.js
@@ -1,52 +1,56 @@
-const { exec } = require('child_process');
+const { promisify } = require('util');
+const { cbExec } = require('child_process');
 
 const checkShouldFireWebHook = require('./check-should-fire-webhook');
 
 const { REPO_OWNER, REPO_NAME, REPO_COMPOSE_PATH, SERVER_DEPLOY_PATH } = require('./constants');
 
+const exec = promisify(cbExec);
+
 function fireWebhook(script, payload) {
-  // Check whether we should fire
-  if (!checkShouldFireWebHook(payload)) {
-    return;
-  }
-
-  // Our tag format contains `-XX` at the end, where XX is Travis' build number.
-  // We need to strip this off to get the GitHub branch name. Furthermore, we
-  // need to account for builds triggered off of a branch, not a tag.
-
-  let githubBranch;
-
-  try {
-    // For branches, this is relatively straightforward--we'll just pull the
-    // branch from the payload.
-    if (!payload.tag) {
-      githubBranch = payload.branch;
-    } else {
-      // But for tags, we'll have to strip the -XX off of the tag.
-      const lastDash = payload.tag.lastIndexOf('-');
-      githubBranch = payload.tag.substring(0, lastDash);
+  return new Promise((resolve, reject) => {
+    // Check whether we should fire
+    if (!checkShouldFireWebHook(payload)) {
+      reject(new Error('This webhook should not be fired.'));
     }
-  } catch (err) {
-    console.error('Error getting branch name:');
-    console.error(err);
-  }
 
-  const composeFileURL = `https://raw.githubusercontent.com/${REPO_OWNER}/${REPO_NAME}/${githubBranch}/${REPO_COMPOSE_PATH}`;
+    // Our tag format contains `-XX` at the end, where XX is Travis' build number.
+    // We need to strip this off to get the GitHub branch name. Furthermore, we
+    // need to account for builds triggered off of a branch, not a tag.
 
-  // Be very careful! The order of these arguments is vital
-  exec(
-    `bash ${script} ${
-      payload.tag
-    } ${composeFileURL} ${SERVER_DEPLOY_PATH} ${REPO_OWNER} ${REPO_NAME}`,
-    (error, stdout, stderr) => {
-      if (error) {
-        console.error(`exec error: ${error}`);
-        return;
+    let githubBranch;
+
+    try {
+      // For branches, this is relatively straightforward--we'll just pull the
+      // branch from the payload.
+      if (!payload.tag) {
+        githubBranch = payload.branch;
+      } else {
+        // But for tags, we'll have to strip the -XX off of the tag.
+        const lastDash = payload.tag.lastIndexOf('-');
+        githubBranch = payload.tag.substring(0, lastDash);
       }
-      console.log(`stdout: ${stdout}`);
-      console.error(`stderr: ${stderr}`);
+    } catch (err) {
+      console.error('Error getting branch name.');
+      reject(err);
     }
-  );
+
+    const composeFileURL = `https://raw.githubusercontent.com/${REPO_OWNER}/${REPO_NAME}/${githubBranch}/${REPO_COMPOSE_PATH}`;
+
+    // Be very careful! The order of these arguments is vital
+    exec(
+      `bash ${script} ${
+        payload.tag
+      } ${composeFileURL} ${SERVER_DEPLOY_PATH} ${REPO_OWNER} ${REPO_NAME}`
+    )
+      .catch(err => {
+        reject(err);
+      })
+      .then((stdout, stderr) => {
+        if (stderr) console.error(`stderr: ${stderr}`);
+        resolve(stdout);
+      });
+  });
 }
 
 module.exports = fireWebhook;
