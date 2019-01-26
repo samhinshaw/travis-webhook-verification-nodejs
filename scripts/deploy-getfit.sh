c57721bc7fa69f96fc76afd0f2dc9d087b9e228a
Sam Hinshaw <samuel.hinshaw@gmail.com>
2019-01-25 20:13:38 -0800

    rename variables in bash script

--- scripts/deploy-getfit.sh
+++ scripts/deploy-getfit.sh
@@ -5,19 +5,19 @@ set -e
 
 # Be very careful! The order of these arguments is vital
 TAG=$1
-COMPUSE_FILE_URL=$2
+COMPOSE_FILE_URL=$2
 SERVER_DEPLOY_PATH=$3
 REPO_OWNER=$4
 REPO_NAME=$5
 
 # Make deploy dir if not already existing
-mkdir -p $DIR 
+mkdir -p $SERVER_DEPLOY_PATH 
 
 # Get the correct docker compose file
-wget -q $URL -O $DIR/docker-compose.yml
+wget -q $COMPOSE_FILE_URL -O $SERVER_DEPLOY_PATH/docker-compose.yml
 
 # Pull down the tagged image
 docker pull $REPO_OWNER/$REPO_NAME:$TAG
 
 # boot it up!
-docker-compose -f $DIR/docker-compose.yml up -d
+docker-compose -f $SERVER_DEPLOY_PATH/docker-compose.yml up -d
