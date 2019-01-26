a1b6baee871d3c8a52439b8f5f68380411bbb7ff
Sam Hinshaw <samuel.hinshaw@gmail.com>
2019-01-25 23:53:25 -0800

    Make sure to trim branch name for tag

--- travis/fire-webhook.js
+++ travis/fire-webhook.js
@@ -14,10 +14,28 @@ function fireWebhook(script, payload) {
 
     // Our Docker tag format contains `-XX` at the end (where XX is Travis' build number).
     // We need to add this to the GitHub branch name or tag.
+    // Additionally, if our tag originates from a github branch, remove any prefix.
 
-    const githubBranch = payload.tag ? payload.tag : payload.branch;
-
-    const dockerTag = `${githubBranch}-${payload.number}`;
+    let githubBranch;
+    let dockerTag;
+    try {
+      if (payload.tag) {
+        githubBranch = payload.tag;
+        dockerTag = `${payload.tag}-${payload.number}`;
+        // Otherwise, we're dealing with a build from a branch:
+      } else {
+        githubBranch = payload.branch;
+        // First make the branch without the prefix (release/release-name -> release-name).
+        // Awesomely, this works when a '/' isn't present either, as indexOf
+        // returns -1 if the specified character isn't present, meaning we
+        // substring from (-1 + 1 = 0) to the end of the string!
+        const cutBranch = payload.branch.substring(payload.branch.indexOf('/') + 1);
+        // then construct the docker tag
+        dockerTag = `${cutBranch}-${payload.number}`;
+      }
+    } catch (err) {
+      reject(err);
+    }
 
     const composeFileURL = `https://raw.githubusercontent.com/${REPO_OWNER}/${REPO_NAME}/${githubBranch}/${REPO_COMPOSE_PATH}`;
 
