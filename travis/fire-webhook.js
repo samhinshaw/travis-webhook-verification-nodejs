77115c3aa3f210ce01db5beb8a087a30202f2920
Sam Hinshaw <samuel.hinshaw@gmail.com>
2019-02-23 17:21:07 -0800

    Make sure nothing runs if hook shouldn't fire

--- travis/fire-webhook.js
+++ travis/fire-webhook.js
@@ -11,43 +11,43 @@ function fireWebhook(script, payload) {
       resolve({
         failure: `This webhook should not be fired.\nBranch: ${payload.branch}\nTag: ${payload.tag}`
       });
-    }
+    } else {
+      // Our Docker tag format contains `-XX` at the end (where XX is Travis' build number).
+      // We need to add this to the GitHub branch name or tag.
+      // Additionally, if our tag originates from a github branch, remove any prefix.
 
-    // Our Docker tag format contains `-XX` at the end (where XX is Travis' build number).
-    // We need to add this to the GitHub branch name or tag.
-    // Additionally, if our tag originates from a github branch, remove any prefix.
-
-    let githubBranch;
-    let dockerTag;
-    try {
-      if (payload.tag) {
-        githubBranch = payload.tag;
-        dockerTag = `${payload.tag}-${payload.number}`;
-        // Otherwise, we're dealing with a build from a branch:
-      } else {
-        githubBranch = payload.branch;
-        // First make the branch without the prefix (release/release-name -> release-name).
-        // Awesomely, this works when a '/' isn't present either, as indexOf
-        // returns -1 if the specified character isn't present, meaning we
-        // substring from (-1 + 1 = 0) to the end of the string!
-        const cutBranch = payload.branch.substring(payload.branch.indexOf(`/`) + 1);
-        // then construct the docker tag
-        dockerTag = `${cutBranch}-${payload.number}`;
+      let githubBranch;
+      let dockerTag;
+      try {
+        if (payload.tag) {
+          githubBranch = payload.tag;
+          dockerTag = `${payload.tag}-${payload.number}`;
+          // Otherwise, we're dealing with a build from a branch:
+        } else {
+          githubBranch = payload.branch;
+          // First make the branch without the prefix (release/release-name -> release-name).
+          // Awesomely, this works when a '/' isn't present either, as indexOf
+          // returns -1 if the specified character isn't present, meaning we
+          // substring from (-1 + 1 = 0) to the end of the string!
+          const cutBranch = payload.branch.substring(payload.branch.indexOf(`/`) + 1);
+          // then construct the docker tag
+          dockerTag = `${cutBranch}-${payload.number}`;
+        }
+      } catch (err) {
+        reject(err);
       }
-    } catch (err) {
-      reject(err);
+
+      const composeFileURL = `https://raw.githubusercontent.com/${REPO_OWNER}/${REPO_NAME}/${githubBranch}/${REPO_COMPOSE_PATH}`;
+
+      // Be very careful! The order of these arguments is vital
+      exec(
+        `bash ${script} ${dockerTag} ${composeFileURL} ${SERVER_DEPLOY_PATH} ${REPO_OWNER} ${REPO_NAME}`,
+        (error, stdout, stderr) => {
+          if (error) reject(error);
+          resolve({ stdout, stderr });
+        }
+      );
     }
-
-    const composeFileURL = `https://raw.githubusercontent.com/${REPO_OWNER}/${REPO_NAME}/${githubBranch}/${REPO_COMPOSE_PATH}`;
-
-    // Be very careful! The order of these arguments is vital
-    exec(
-      `bash ${script} ${dockerTag} ${composeFileURL} ${SERVER_DEPLOY_PATH} ${REPO_OWNER} ${REPO_NAME}`,
-      (error, stdout, stderr) => {
-        if (error) reject(error);
-        resolve({ stdout, stderr });
-      }
-    );
   });
 }
 
